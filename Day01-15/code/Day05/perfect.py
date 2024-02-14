ffd068de4baa0f748b077acdf8dec144f5d1ca46
abbcadd <154320385+abbcadd@users.noreply.github.com>
2024-02-14 18:41:00 +0800

    Update perfect.py
    这种写法应该也可以

--- Day01-15/code/Day05/perfect.py
+++ Day01-15/code/Day05/perfect.py
@@ -18,3 +18,12 @@ for num in range(2, 10000):
                 result += num // factor
     if result == num:
         print(num)
+        
+for num in range(1, 10001):
+    sum = 0
+    for i in range(1, num):
+        if num % i == 0:
+            sum += i
+    if sum == num:
+        print(num)
+
